5c6c8dca9054739fa4fb3a61ccbd713e6b999ff5
PJalv <pjalvbusiness@gmail.com>
2024-10-07 20:57:13 -0700

    create hal.zig

---
+++ hal.zig
@@ -0,0 +1,210 @@
+pub const rcc = extern struct {
+    CR: u32,
+    PLLCFGR: u32,
+    CFGR: u32,
+    CIR: u32,
+    AHB1RSTR: u32,
+    AHB2RSTR: u32,
+    AHB3RSTR: u32,
+    RESERVED0: u32,
+    APB1RSTR: u32,
+    APB2RSTR: u32,
+    RESERVED1: [2]u32,
+    AHB1ENR: u32,
+    AHB2ENR: u32,
+    AHB3ENR: u32,
+    RESERVED2: u32,
+    APB1ENR: u32,
+    APB2ENR: u32,
+    RESERVED3: [2]u32,
+    AHB1LPENR: u32,
+    AHB2LPENR: u32,
+    AHB3LPENR: u32,
+    RESERVED4: u32,
+    APB1LPENR: u32,
+    APB2LPENR: u32,
+    RESERVED5: [2]u32,
+    BDCR: u32,
+    CSR: u32,
+    RESERVED6H: u32,
+    RESERVEDK: u32,
+    SSCGR: u32,
+    PLLI2SCFGR: u32,
+};
+pub const FREQ = 16000000;
+
+pub const RCC: *volatile rcc = @ptrFromInt(0x40023800);
+const UART1: *volatile uart = @ptrFromInt(0x40011000);
+const UART2: *volatile uart = @ptrFromInt(0x40004400);
+pub const UART3: *volatile uart = @ptrFromInt(0x40004800);
+
+const SYSTICK: *volatile systick = @ptrFromInt(0xe000e010);
+const GPIOB: *volatile gpio = @ptrFromInt(0x40020400);
+
+pub const GPIO_MODE = enum(u3) { INPUT, OUTPUT, AF, ANALOG };
+const gpio = extern struct {
+    MODER: u32,
+    OTYPER: u32,
+    OSPEEDR: u32,
+    PUPDR: u32,
+    IDR: u32,
+    ODR: u32,
+    BSRR: u32,
+    LCKR: u32,
+    AFR: [2]u32,
+};
+const systick = extern struct {
+    CTRL: u32,
+    LOAD: u32,
+    VAL: u32,
+    CALIB: u32,
+};
+
+const uart = extern struct {
+    SR: u32,
+    DR: u32,
+    BRR: u32,
+    CR1: u32,
+    CR2: u32,
+    CR3: u32,
+    GTPR: u32,
+};
+
+pub inline fn uart_init(uart_: *volatile uart, baud: u32) void {
+    // https://www.st.com/resource/en/datasheet/stm32f429zi.pdf
+    const af: u8 = 7; // Alternate function
+    var rx: u16 = 0;
+    var tx: u16 = 0; // pins
+
+    if (uart_ == UART1)
+        RCC.APB2ENR |= BIT(4);
+    if (uart_ == UART2)
+        RCC.APB1ENR |= BIT(17);
+    if (uart_ == UART3)
+        RCC.APB1ENR |= BIT(18);
+
+    if (uart_ == UART1)
+        tx = PIN('A', 9);
+    rx = PIN('A', 10);
+    if (uart_ == UART2)
+        tx = PIN('A', 2);
+    rx = PIN('A', 3);
+    if (uart_ == UART3)
+        tx = PIN('D', 8);
+    rx = PIN('D', 9);
+
+    gpio_set_mode(tx, GPIO_MODE.AF);
+    gpio_set_af(tx, af);
+    gpio_set_mode(rx, GPIO_MODE.AF);
+    gpio_set_af(rx, af);
+    uart_.CR1 = 0; // Disable this UART
+    uart_.BRR = FREQ / baud; // FREQ is a UART bus frequency
+    uart_.CR1 |= BIT(13) | BIT(2) | BIT(3); // Set UE, RE, TE
+}
+pub inline fn uart_read_ready(uart_: *volatile uart) u32 {
+    return uart_.SR & BIT(5); // If RXNE bit is set, data is ready
+}
+pub inline fn uart_read_byte(self: *volatile uart) u8 {
+    return @as(u8, self.DR & 255);
+}
+
+pub inline fn uart_write_byte(uart_: *volatile uart, byte: u8) void {
+    uart_.DR = byte;
+    while ((uart_.SR & BIT(7)) == 0)
+        delay_loop(1);
+}
+pub inline fn uart_write_buf(uart_: *volatile uart, buf: []const u8) void {
+    for (buf) |char| {
+        uart_write_byte(uart_, char);
+    }
+}
+// pub inline Functions
+//
+pub inline fn gpio_set_mode(pin: u16, mode: GPIO_MODE) void {
+    const gpio_: *gpio = GPIO(PINBANK(pin));
+    const n: u16 = PINNO(pin);
+    RCC.AHB1ENR |= BIT(PINBANK(pin));
+    const mask: u32 = @as(u32, 3) << @intCast((n * 2));
+    const mode_value: u32 = @as(u32, (@intFromEnum(mode) & 3)) << @intCast((n * 2));
+    gpio_.MODER &= ~mask;
+    gpio_.MODER |= mode_value;
+}
+
+pub inline fn gpio_set_af(pin: u16, af_num: u8) void {
+    const gpio_: *gpio = GPIO(PINBANK(pin));
+    const n: u16 = PINNO(pin);
+    RCC.AHB1ENR |= BIT(PINBANK(pin));
+    gpio_.AFR[n >> 3] &= ~(@as(u32, 15) << @intCast(((n & 7) * 4)));
+    gpio_.AFR[n >> 3] |= (@as(u32, af_num) << (@intCast((n & 7) * 4)));
+}
+
+pub inline fn gpio_write(pin: u16, val: bool) void {
+    const gpio_: *gpio = GPIO(PINBANK(pin));
+    const bsr_value: u32 = @as(u32, 1) << @as(u32, PINNO(pin)) << @as(u5, (if (val) 0 else 16));
+    gpio_.BSRR = bsr_value;
+}
+pub inline fn gpio_read(pin: u16) bool {
+    const gpio_: *gpio = GPIO(PINBANK(pin));
+    const idr_value: u32 = gpio_.IDR; // Read the Input Data Register
+    // Get the pin number and mask the corresponding bit
+    return ((idr_value & @as(u32, 1) << @as(u32, PINNO(pin))) != 0);
+}
+
+fn delay_loop(iterations: usize) void {
+    var count: usize = iterations;
+    while (count != 0) : (count -= 1) {
+        asm volatile ("nop");
+    }
+}
+
+pub var counter: u32 = 0;
+
+pub fn get_counter() u32 {
+    return @as(*volatile u32, @ptrCast(&counter)).*;
+}
+pub fn SysTick_Handler() callconv(.C) void {
+    counter += 1;
+}
+pub fn timer_expired(t: *u32, prd: u32, now: u32) bool {
+    if (now + prd < t.*)
+        t.* = 0; // Time wrapped? Reset timer
+    if (t.* == 0)
+        t.* = now + prd; // First poll? Set expiration
+    if (t.* > now)
+        return false; // Not expired yet, return
+    t.* = if ((now - t.*) > prd) now + prd else t.* + prd; // Next expiration time
+    return true; // Expired, return true
+}
+
+pub inline fn systick_init(comptime ticks: u32) void {
+    if ((ticks - 1) > 0xffffff)
+        return; // Systick timer is 24 bit
+    SYSTICK.LOAD = ticks - 1;
+    SYSTICK.VAL = 0;
+    SYSTICK.CTRL = BIT(0) | BIT(1) | BIT(2); // Enable systick
+    RCC.APB2ENR |= BIT(14); // Enable SYSCFG
+}
+
+pub inline fn GPIO(bank: u32) *gpio {
+    return @ptrFromInt((0x40020000 + 0x400 * (bank)));
+}
+
+pub inline fn PINBANK(pin: u16) u32 {
+    const bank = pin >> 8;
+    return bank;
+}
+
+pub inline fn PINNO(pin: u16) u16 {
+    const number = pin & 255;
+    return number;
+}
+
+pub inline fn BIT(x: u32) u32 {
+    const bit_value = @as(c_ulong, 1) << @intCast(x);
+    return bit_value;
+}
+
+pub inline fn PIN(comptime bank: u32, comptime num: u32) u16 {
+    const pin_value = ((bank - 'A') << 8) | num;
+    return pin_value;
+}
--- main.zig
+++ main.zig
@@ -1,151 +1,26 @@
-const std = @import("std");
-const RCC = @import("rcc.zig").RCC;
+const hal = @import("hal.zig");
 
-// Constants and Struct Definitions
+// Main Function
+pub fn main() void {
+    hal.systick_init(hal.FREQ / 1000);
+    hal.uart_init(hal.UART3, 115200);
+    const period: u32 = 1000;
+    var timer: u32 = 0;
+    const led: u16 = hal.PIN('B', 7);
+    const button: u16 = hal.PIN('C', 13);
+    hal.gpio_set_mode(led, hal.GPIO_MODE.OUTPUT);
+    hal.gpio_set_mode(button, hal.GPIO_MODE.INPUT);
 
-const gpio = extern struct {
-    MODER: u32,
-    OTYPER: u32,
-    OSPEEDR: u32,
-    PUPDR: u32,
-    IDR: u32,
-    ODR: u32,
-    BSRR: u32,
-    LCKR: u32,
-    AFR: [2]u32,
-};
+    var on: bool = true;
 
-const systick = extern struct {
-    CTRL: u32,
-    LOAD: u32,
-    VAL: u32,
-    CALIB: u32,
-};
-
-const uart = extern struct {
-    SR: u32,
-    DR: u32,
-    BRR: u32,
-    CR1: u32,
-    CR2: u32,
-    CR3: u32,
-    GTPR: u32,
-};
-
-pub inline fn uart_init(self: *volatile uart, baud: u32) void {
-    // https://www.st.com/resource/en/datasheet/stm32f429zi.pdf
-    const af: u8 = 7; // Alternate function
-    var rx: u16 = 0;
-    var tx: u16 = 0; // pins
-
-    if (self == UART1)
-        RCC.APB2ENR |= BIT(4);
-    if (self == UART2)
-        RCC.APB1ENR |= BIT(17);
-    if (self == UART3)
-        RCC.APB1ENR |= BIT(18);
-
-    if (self == UART1)
-        tx = PIN('A', 9);
-    rx = PIN('A', 10);
-    if (self == UART2)
-        tx = PIN('A', 2);
-    rx = PIN('A', 3);
-    if (self == UART3)
-        tx = PIN('D', 8);
-    rx = PIN('D', 9);
-
-    gpio_set_mode(tx, GPIO_MODE.AF);
-    gpio_set_af(tx, af);
-    gpio_set_mode(rx, GPIO_MODE.AF);
-    gpio_set_af(rx, af);
-    self.CR1 = 0; // Disable this UART
-    self.BRR = FREQ / baud; // FREQ is a UART bus frequency
-    self.CR1 |= BIT(13) | BIT(2) | BIT(3); // Set UE, RE, TE
-}
-pub inline fn uart_read_ready(uart_: *volatile uart) u32 {
-    return uart_.SR & BIT(5); // If RXNE bit is set, data is ready
-}
-pub inline fn uart_read_byte(self: *volatile uart) u8 {
-    return @as(u8, self.DR & 255);
-}
-
-pub inline fn uart_write_byte(uart_: *volatile uart, byte: u8) void {
-    uart_.DR = byte;
-    while ((uart_.SR & BIT(7)) == 0)
-        delay_loop(1);
-}
-pub inline fn uart_write_buf(uart_: *volatile uart, buf: []const u8) void {
-    for (buf) |char| {
-        uart_write_byte(uart_, char);
+    while (true) {
+        if (hal.timer_expired(&timer, period, hal.get_counter())) {
+            hal.gpio_write(led, on);
+            on = !on;
+            hal.uart_write_buf(hal.UART3, "LIGHTNING MCQUEEN\r\n");
+        }
+        hal.gpio_write(led, hal.gpio_read(button));
     }
-}
-const FREQ = 16000000;
-
-const UART1: *volatile uart = @ptrFromInt(0x40011000);
-const UART2: *volatile uart = @ptrFromInt(0x40004400);
-const UART3: *volatile uart = @ptrFromInt(0x40004800);
-
-const SYSTICK: *volatile systick = @ptrFromInt(0xe000e010);
-const GPIOB: *volatile gpio = @ptrFromInt(0x40020400);
-
-const GPIO_MODE = enum(u3) { INPUT, OUTPUT, AF, ANALOG };
-
-// Inline Functions
-inline fn gpio_set_mode(pin: u16, mode: GPIO_MODE) void {
-    const gpio_: *gpio = GPIO(PINBANK(pin));
-    const n: u16 = PINNO(pin);
-    RCC.AHB1ENR |= BIT(PINBANK(pin));
-    const mask: u32 = @as(u32, 3) << @intCast((n * 2));
-    const mode_value: u32 = @as(u32, (@intFromEnum(mode) & 3)) << @intCast((n * 2));
-    gpio_.MODER &= ~mask;
-    gpio_.MODER |= mode_value;
-}
-
-inline fn gpio_set_af(pin: u16, af_num: u8) void {
-    const gpio_: *gpio = GPIO(PINBANK(pin));
-    const n: u16 = PINNO(pin);
-    RCC.AHB1ENR |= BIT(PINBANK(pin));
-    gpio_.AFR[n >> 3] &= ~(@as(u32, 15) << @intCast(((n & 7) * 4)));
-    gpio_.AFR[n >> 3] |= (@as(u32, af_num) << (@intCast((n & 7) * 4)));
-}
-
-inline fn gpio_write(pin: u16, val: bool) void {
-    const gpio_: *gpio = GPIO(PINBANK(pin));
-    const bsr_value: u32 = @as(u32, 1) << @as(u32, PINNO(pin)) << @as(u5, (if (val) 0 else 16));
-    gpio_.BSRR = bsr_value;
-}
-
-fn delay_loop(iterations: usize) void {
-    var count: usize = iterations;
-    while (count != 0) : (count -= 1) {
-        asm volatile ("nop");
-    }
-}
-
-var counter: u32 = 0;
-
-fn SysTick_Handler() callconv(.C) void {
-    counter += 1;
-}
-fn timer_expired(t: *u32, prd: u32, now: u32) bool {
-    if (now + prd < t.*)
-        t.* = 0; // Time wrapped? Reset timer
-    if (t.* == 0)
-        t.* = now + prd; // First poll? Set expiration
-    if (t.* > now)
-        return false; // Not expired yet, return
-    t.* = if ((now - t.*) > prd) now + prd else t.* + prd; // Next expiration time
-    return true; // Expired, return true
-}
-
-inline fn systick_init(comptime ticks: u32) void {
-    if ((ticks - 1) > 0xffffff)
-        return; // Systick timer is 24 bit
-    SYSTICK.LOAD = ticks - 1;
-    SYSTICK.VAL = 0;
-    SYSTICK.CTRL = BIT(0) | BIT(1) | BIT(2); // Enable systick
-    RCC.APB2ENR |= BIT(14); // Enable SYSCFG
 }
 
 export fn _start() noreturn {
@@ -153,49 +28,6 @@ export fn _start() noreturn {
 
     unreachable;
 }
-
-inline fn GPIO(bank: u32) *gpio {
-    return @ptrFromInt((0x40020000 + 0x400 * (bank)));
-}
-
-inline fn PINBANK(pin: u16) u32 {
-    const bank = pin >> 8;
-    return bank;
-}
-
-inline fn PINNO(pin: u16) u16 {
-    const number = pin & 255;
-    return number;
-}
-
-inline fn BIT(x: u32) u32 {
-    const bit_value = @as(c_ulong, 1) << @intCast(x);
-    return bit_value;
-}
-
-inline fn PIN(comptime bank: u32, comptime num: u32) u16 {
-    const pin_value = ((bank - 'A') << 8) | num;
-    return pin_value;
-}
-// Main Function
-pub fn main() void {
-    systick_init(16_000_000 / 1000);
-    uart_init(UART3, 115200); // Initialise UART
-    const period: u32 = 1000;
-    var timer: u32 = 0;
-    const led: u16 = PIN('B', 7);
-    gpio_set_mode(led, GPIO_MODE.OUTPUT);
-    var on: bool = true;
-
-    while (true) {
-        if (timer_expired(&timer, period, @as(*volatile u32, @ptrCast(&counter)).*)) {
-            gpio_write(led, on);
-            on = !on;
-            uart_write_buf(UART3, "HELLO FROM ZIG OMG\r\n");
-        }
-    }
-}
-
 // Reset Function
 extern fn _estack() void;
 
@@ -223,4 +55,4 @@ fn _reset() callconv(.C) noreturn {
 }
 fn _zero() callconv(.C) void {}
 // Vector Table
-export const vector_table linksection(".vectors") = [_]*const fn () callconv(.C) void{ _estack, _reset, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, SysTick_Handler };
+export const vector_table linksection(".vectors") = [_]*const fn () callconv(.C) void{ _estack, _reset, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, _zero, hal.SysTick_Handler };
